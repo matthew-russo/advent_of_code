663f1826bd2fe1e65ebe1adcbf11cfe099084c80
Matthew Russo <matthew@matthewclayrusso.com>
2019-12-07 18:42:12 -0800

    racket solution to day 6 - part 2

---
+++ 2019/day_6/racket/part_2.rkt
@@ -0,0 +1,91 @@
+#lang racket
+
+(struct graph (nodes))
+(struct node (id children))
+
+(define (init-graph)
+  (let* ([init-node (node "COM" (list))]
+         [nodes (make-hash (list (cons "COM" init-node)))])
+    (graph nodes)))
+
+(define (add-node-to-graph g n)
+  (let ([nodes (graph-nodes g)]
+        [id    (node-id n)])
+    (if (hash-has-key? nodes id)
+        (let* ([prev-node    (hash-ref nodes id)]
+               [new-children (append (node-children prev-node) (node-children n))]
+               [new-node (node id new-children)])
+          (hash-set! nodes id new-node)
+          (graph nodes))
+        (begin
+          (hash-set! nodes id n)
+          (graph nodes)))))
+
+(define (count-orbits-graph g)
+  (let ([init-node (hash-ref (graph-nodes g) "COM")])
+    (count-orbits-node g init-node 0)))
+
+(define (count-orbits-node g n current-depth)
+  (let* ([next-depth         (+ current-depth 1)]
+         [children           (node-children n)]
+         [count-child-orbits (lambda (n1)
+                               (if (hash-has-key? (graph-nodes g) n1)
+                                   (count-orbits-node g (hash-ref (graph-nodes g) n1) next-depth)
+                                   next-depth))]
+         [children-orbits    (map count-child-orbits children)]
+         [full-list          (append (list current-depth) children-orbits)])
+    (apply + full-list)))
+
+(define (dfs g looking-for current path)
+  (if (equal? looking-for current)
+      path
+      (if (not (hash-has-key? (graph-nodes g) current))
+          (list)
+          (let* ([n         (hash-ref (graph-nodes g) current)]
+                 [children  (node-children n)]
+                 [new-path  (append path (list current))])
+            (flatten (map (lambda (c) (dfs g looking-for c new-path)) children))))))
+ 
+(define (path-to g dst)
+  (dfs g dst "COM" (list)))
+
+(define (-last-common-parent path-one path-two last)
+  (let ([n1    (car path-one)]
+        [n2    (car path-two)]
+        [rest1 (cdr path-one)]
+        [rest2 (cdr path-two)])
+    (if (equal? n1 n2)
+        (-last-common-parent rest1 rest2 n1)
+        last)))
+
+(define (last-common-parent path-one path-two)
+  (-last-common-parent path-one path-two "###ROOT###"))
+
+(define (dist-from parent path)
+  (let ([parent-index (index-of path parent)])
+    (- (length path) (+ 1 parent-index))))
+
+(define (parse-line-to-node line)
+  (let* ([split (string-split line ")")]
+         [id    (list-ref split 0)]
+         [child (list-ref split 1)])
+    (node id (list child))))
+
+(define (add-line-to-graph g line)
+  (add-node-to-graph g (parse-line-to-node line)))
+
+(define (graph-from-input)
+  (foldl (lambda (line g) (add-line-to-graph g line))
+         (init-graph)
+         (sequence->list (in-lines))))
+
+(define (solution)
+  (let* ([g (graph-from-input)]
+         [path-to-you   (path-to g "YOU")]
+         [path-to-san   (path-to g "SAN")]
+         [common-parent (last-common-parent path-to-you path-to-san)]
+         [you-dist      (dist-from common-parent path-to-you)]
+         [san-dist      (dist-from common-parent path-to-san)])
+    (println (number->string (+ you-dist san-dist)))))
+
+(solution)
