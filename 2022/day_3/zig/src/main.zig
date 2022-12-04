2b2f04a62575b225ee78b8a26520d680580a99a2
Matthew Russo <matthew@matthewclayrusso.com>
2022-12-03 22:23:16 -0800

    [2022] Day 3 : adds solution for second half, finding the common item between each 3 lines

--- 2022/day_3/zig/src/main.zig
+++ 2022/day_3/zig/src/main.zig
@@ -7,36 +7,43 @@ pub fn main() !void {
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    var first_half_char_present: [52]bool = [_]bool{false} ** 52;
-    var second_half_char_present: [52]bool = [_]bool{false} ** 52;
+    var first_elf_char_present: [52]bool = [_]bool{false} ** 52;
+    var second_elf_char_present: [52]bool = [_]bool{false} ** 52;
+    var third_elf_char_present: [52]bool = [_]bool{false} ** 52;
     var total_priority: u64 = 0;
 
     var buf: [1024]u8 = undefined;
+    var lines_processed: u64 = 0;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        if (line.len % 2 != 0) {
-            std.debug.panic("line isn't even", .{});
-        }
-
-        first_half_char_present = [_]bool{false} ** 52;
-        second_half_char_present = [_]bool{false} ** 52;
-
-        var half = line.len / 2;
-        for (line) |c, idx| {
+        for (line) |c| {
             var priority = charToPriority(c) - 1;
-            if (idx < half) {
-                first_half_char_present[priority] = true;
+            if (lines_processed % 3 == 0) {
+                first_elf_char_present[priority] = true;
+            } else if (lines_processed % 3 == 1) {
+                second_elf_char_present[priority] = true;
+            } else if (lines_processed % 3 == 2) {
+                third_elf_char_present[priority] = true;
             } else {
-                second_half_char_present[priority] = true;
+                unreachable;
             }
         }
 
-        var idx: u64 = 0;
-        while (idx < 52) {
-            if (first_half_char_present[idx] and second_half_char_present[idx]) {
-                total_priority += idx + 1;
+        if (lines_processed % 3 == 2) {
+            var idx: u64 = 0;
+            while (idx < 52) {
+                if (first_elf_char_present[idx] and second_elf_char_present[idx] and third_elf_char_present[idx]) {
+                    total_priority += idx + 1;
+                }
+                idx += 1;
             }
-            idx += 1;
+
+            // clear our counters
+            first_elf_char_present = [_]bool{false} ** 52;
+            second_elf_char_present = [_]bool{false} ** 52;
+            third_elf_char_present = [_]bool{false} ** 52;
         }
+
+        lines_processed += 1;
     }
 
     std.log.info("total priority is {d}", .{total_priority});
