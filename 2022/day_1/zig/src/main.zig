3373866419298fd93cc80e7905fa07f1f2bca36a
Matthew Russo <matthew@matthewclayrusso.com>
2022-12-03 17:19:17 -0800

    [2022] Day 1 : adds second half of solution, tracking top 3 elves

--- 2022/day_1/zig/src/main.zig
+++ 2022/day_1/zig/src/main.zig
@@ -7,7 +7,9 @@ pub fn main() !void {
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    var max: u64 = 0;
+    var highest_one: u64 = 0;
+    var highest_two: u64 = 0;
+    var highest_three: u64 = 0;
     var curr: u64 = 0;
 
     var buf: [1024]u8 = undefined;
@@ -16,12 +18,23 @@ pub fn main() !void {
             var n = try std.fmt.parseInt(u64, line, 10);
             curr += n;
         } else {
-            if (curr > max) {
-                max = curr;
+            if (curr >= highest_one) {
+                var old_highest_one = highest_one;
+                var old_highest_two = highest_two;
+                highest_one = curr;
+                highest_two = old_highest_one;
+                highest_three = old_highest_two;
+            } else if (curr >= highest_two) {
+                var old_highest_two = highest_two;
+                highest_two = curr;
+                highest_three = old_highest_two;
+            } else if (curr > highest_three) {
+                highest_three = curr;
             }
             curr = 0;
         }
     }
 
-    std.log.info("max is {d}", .{max});
+    std.log.info("top 1 is {d}", .{highest_one});
+    std.log.info("top 3 are {d}", .{highest_one + highest_two + highest_three});
 }
